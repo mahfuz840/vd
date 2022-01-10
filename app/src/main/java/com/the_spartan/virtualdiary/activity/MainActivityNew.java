6e311f1b62031adf2d235e25c6091cbea2e2eeea
Mahfuz Ahmed <mahfuz.ahmed@therapservices.net>
2022-01-10 21:31:25 +0600

    Implemented home page UI

---
+++ app/src/main/java/com/the_spartan/virtualdiary/activity/MainActivityNew.java
@@ -0,0 +1,16 @@
+package com.the_spartan.virtualdiary.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.the_spartan.virtualdiary.R;
+
+public class MainActivityNew extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_new);
+    }
+}
